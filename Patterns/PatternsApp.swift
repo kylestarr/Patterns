0c6c4bcc834d142e1e27af333c4b16707d2e50a3
Kyle Starr <kyle.starr@icloud.com>
2022-05-19 17:43:55 -0700

    Initial Commit

---
+++ Patterns/PatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatternsApp.swift
+//  Patterns
+//
+//  Created by Kyle Starr on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct PatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
